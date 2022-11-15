33f479bf98d6346c532380ce10959e718c1a0197
birdaNFT <birdaNFT@gmail.com>
2022-11-15 01:19:44 +0000

    Added to-do lists

---
+++ app/todos/[todoId]/not-found.tsx
@@ -0,0 +1,5 @@
+function notFound() {
+  return <div>We couldn't find the To-Do you were looking for</div>;
+}
+
+export default notFound;
---
+++ app/todos/page.tsx
@@ -0,0 +1,12 @@
+import TodosList from "./TodosList";
+
+function Todos() {
+  return (
+    <div>
+      {/* @ts-ignore */}
+      <TodosList />
+    </div>
+  );
+}
+
+export default Todos;
