8d5bc5b34cffae1c86f6b28674cb26e181f0ac92
birdaNFT <birdaNFT@gmail.com>
2022-11-16 00:23:07 +0000

    Add todo list to layout

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,10 +1,7 @@
-import TodosList from "./TodosList";
-
 function Todos() {
   return (
     <div>
-      {/* @ts-ignore */}
-      <TodosList />
+      <h1>This is where the Todos will be listed...</h1>
     </div>
   );
 }
